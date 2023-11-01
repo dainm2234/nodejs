7659e9a8a5ac693b741135485028c249575f917d
dainm2234 <dainmph30444@fpt.edu.vn>
2023-11-01 17:46:16 +0700

    day2

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-const http = require('node:http');
+// const http = require("node:http");
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(express.json())
+
+app.get('/products', (req, res) => {
+  res.send('Product page!')
+})
+
+app.post('/products/:slug/:id', (req, res) => {
+  const {params, query, body} = req
+  console.log("params:", params)
+  console.log(query)
+  console.log(body)
+  res.send('Add new product page!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 // Create a local server to receive data from
-const server = http.createServer();
+// const app = http.createServer((req, res) => {
+//   const { url, method} = req
+//   switch (url) {
+//     case "/products":
+//       res.writeHead(200);
+//       res.end(
+//         `<h1>Products Page!</h1>`
+//       );
+//       break;
+//     case "/detail":
+//       res.end(
+//         `<h1>Product detail page</h1>`
+//       )
 
-// Listen to the request event
-server.on('request', (request, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    `
-    <h1> Xin chao ca the gioi! </h1> 
-    `
-  );
-});
+//   }
+  
+// });
 
-server.listen(8000,() => {
-    console.log("Server dang khoi chay")
-}
-);
+// app.listen(8000, () => {
+//   console.log("Server đang chạy trên PORT 8000");
+// });
---
+++ note/day2.md
@@ -0,0 +1,14 @@
+` npm i express`
+
+-Có 3 cách truyền dữ liệu : req.params, req.query, req.body
+
+`http://localhost:8000/products/:slug` - params
+
+`http://localhost:8000/products?color=red` -query
+
+-body
+
+{
+    "name": "iphone 15",
+    "price": 2000
+}  
